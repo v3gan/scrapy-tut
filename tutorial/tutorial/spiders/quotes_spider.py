6b3b3d456491435c88467ae23a10bc4577decc8f
Marc Bowman <marabow@pm.me>
2025-12-01 13:04:04 -0500

    adds  furst spider and results of run

---
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import scrapy
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    async def start(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f"quotes-{page}.html"
+        Path(filename).write_bytes(response.body)
+        self.log(f"Saved file {filename}")
